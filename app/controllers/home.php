9201118e5f6d0915a4aaf32727b295904f5d5421
eroritoo <erorat10@gmail.com>
2022-09-28 04:42:53 +0200

    new project

---
+++ app/controllers/home.php
@@ -0,0 +1,13 @@
+<?php
+
+class home extends controller{
+
+    public function index($name = '')
+    {
+        $user = $this->model('user');
+        $user->name = $name;
+
+        $this->view('home/index', ['name' => $user->name]);
+    }
+
+}
